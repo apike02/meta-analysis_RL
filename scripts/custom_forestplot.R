10346945c5b1810d193eb0942f907513fb9e9522
Alexandra Pike <alex.pike02@gmail.com>
2021-08-17 16:59:13 +0100

    rerun meta prior to sim

--- scripts/custom_forestplot.R
+++ scripts/custom_forestplot.R
@@ -1,5 +1,5 @@
 custom_forestplot<-function(meta_analysis,fix_or_random){
-  
+
   if (fix_or_random=='fixed'){
     meta_estimate<-meta_analysis$TE.fixed
     meta_lower<-meta_analysis$lower.fixed
@@ -11,18 +11,19 @@ custom_forestplot<-function(meta_analysis,fix_or_random){
     meta_upper<-meta_analysis$upper.random
     label1<-'Random Effects Meta-Analysis'
   }
-  
+
   effectsizes<-data.frame(name=meta_analysis$studlab,
                           estimate=meta_analysis$TE,
                           lower=meta_analysis$lower,
                           upper=meta_analysis$upper)
   #effectsizes<-effectsizes[complete.cases(effectsizes),]
-  
+  effectsizes$name<-as.factor(effectsizes$name)
+
   labels<-c('',label1,rev(unique(levels(effectsizes$name)[effectsizes$name]))) #blank gives space for greater labels
   labelcols<-c('transparent',rep('black',length(labels)-1))
-  
+
   limits<-c(min(c(-0.5,effectsizes$lower,effectsizes$upper),na.rm=TRUE),max(c(0.5,effectsizes$lower,effectsizes$upper),na.rm=TRUE)) #limits of -0.1 and 0.1 or the maximum/minimum values
-  
+
   ggplot(effectsizes,aes(x=name,y=estimate,fill=estimate))+
     geom_hline(yintercept=0,linetype='dashed',alpha=0.5)+
     geom_hline(yintercept=meta_estimate,linetype='dotted',alpha=0.2)+
@@ -34,12 +35,12 @@ custom_forestplot<-function(meta_analysis,fix_or_random){
     labs(x='Analysis')+
     geom_errorbar(aes(x=2,y=meta_estimate,ymin=meta_lower,ymax=meta_upper),size=1,colour='black',show.legend=FALSE,width=0)+
     geom_point(aes(x=2,y=meta_estimate),shape=21,size=2,fill='black')+
-    geom_rect(data=effectsizes[2,],aes(xmin=1.8, xmax=2.2, ymin=meta_lower, ymax=meta_upper),alpha=0.3,fill='forestgreen')+
+    geom_rect(aes(xmin=1.8, xmax=2.2, ymin=meta_lower, ymax=meta_upper),alpha=0.3,fill='forestgreen')+
     scale_x_discrete(limit=labels,labels=labels)+
     theme(axis.text.y = element_text(face = c('plain','bold',rep('plain',nrow(effectsizes)))),axis.ticks.y = element_line(colour = labelcols))+
     #annotate("segment", x = 0.7, xend = 0.7, y = -0.05, yend = 0.05, colour = "grey", size = 1, arrow = arrow(ends='both'))+
     annotate("text", x = 1.2, y = 0.5*limits[2], colour = "grey", label='greater in patients',size=3)+
     annotate("text", x = 1.2, y = 0.5*limits[1], colour = "grey", label='greater in controls',size=3)+
     coord_flip()+
-    ylim(limits) 
+    ylim(limits)
 }
--- scripts/loadRData.R
+++
@@ -1,5 +0,0 @@
-loadRData <- function(fileName){
-  #loads an RData file, and returns it
-  load(fileName)
-  get(ls()[ls() != "fileName"])
-}
